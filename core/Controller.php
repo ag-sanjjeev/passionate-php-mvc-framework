06f6208a677811979b7be0713479eb10145ab02f
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2023-06-28 16:39:53 +0530

    Controller class created and added in Application

---
+++ core/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Controller
+ *
+ * This is a controller class file. Which has control for views
+ *
+ * @copyright 2023 ag-sanjjeev
+ * @license url MIT
+ * @version Release: @1.0@
+ * @link url
+ * @since Class available since Release 1.0
+ */
+class Controller
+{
+	
+	function __construct()
+	{
+		
+	}
+
+	
+}
