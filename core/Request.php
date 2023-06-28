373cb747ba8284e675e115207ca00862d07ffe94
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2023-06-28 12:19:41 +0530

    Added some methods in Request class

--- core/Request.php
+++ core/Request.php
@@ -34,9 +34,16 @@ class Request
 	/**
 	 * The value of all data from request
 	 *
-	 * @var array $inputs
+	 * @var array $input
 	 */
-	public static array $inputs = [];
+	public static array $input = [];
+
+	/**
+	 * The value of all data from request as dynamic property
+	 *
+	 * @var array $d
+	 */
+	public static array $d = [];
 
 	/**
 	 * This will evaluate all request and collects essential information of the request     
@@ -46,22 +53,78 @@ class Request
 	 */
 	public function __construct()
 	{
-		self::$urlPath 			= 	$this->urlRequest();
-		self::$methodRequest	= 	$this->methodRequest();
-		self::$inputs 			= 	$this->inputsFromRequest();
+		self::$urlPath 			= 	$this->url();
+		self::$methodRequest	= 	$this->method();
+		self::$input 			= 	$this->initInput();
+	}
+
+	/**
+	 * Magic method for setting dynamic properties
+	 *	 
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 *
+	 */
+	public function _set($name, $value)
+	{
+		$this->d[$name] = $value;
+	}
+
+	/**
+	 * Magic method for getting dynamic properties
+	 *	 
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return $d[$name]
+	 *
+	 */
+	public function _get($name)
+	{
+		return $this->d[$name] ?? null;
+	}
+
+	/**
+	 * This will collect all data from the request
+	 *
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return $data
+	 *
+	 */
+	public function initInput()
+	{
+		$data = [];
+
+		/*
+			Collects all data if it is a get request method
+			and Sets dynamic property of this class
+		*/
+		if ($this->isGet()) {
+			foreach ($_GET as $key => $value) {
+				$data[$key] = $value;
+				$this->$key = $value;
+			}
+		}
+
+		/*
+			Collects all data if it is a post request method
+			and Sets dynamic property of this class
+		*/
+		if ($this->isPost()) {
+			foreach ($_POST as $key => $value) {
+				$data[$key] = $value;
+				$this->$key = $value;
+			}
+		}		
+
+		return $data;
 	}
 
 	/**
 	 * This will short-out requested url and eliminates any other url parameter
 	 *
-	 * @param directory $path this must be a project directory location	 
-	 * 
-	 * @return urlPath
 	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return urlPath
 	 *
 	 */
-
-	public function urlRequest()
+	public function url()
 	{
 		$urlPath = $_SERVER['REQUEST_URI'] ?? '/';
 
@@ -79,13 +142,108 @@ class Request
 	}
 
 	/**
-	 * This will get information about method of the request
+	 * This will return full url with it's parameters
 	 *
-	 * @return methodRequest
 	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return fullUrlPath
 	 *
 	 */
-	public function methodRequest()
+	public function fullUrl()
+	{
+		$urlPath = $_SERVER['REQUEST_URI'] ?? '/';
+		$serverName = $_SERVER['SERVER_NAME'] ?? '';
+		$serverPort = $_SERVER['SERVER_PORT'] ?? '';
+		
+		$fullUrlPath = $serverName . ":$serverPort" . $urlPath;
+
+		return $fullUrlPath;
+	}
+
+	/**
+	 * This will check whether givern url string matches with the request url
+	 *
+	 * @param url $urlName this is url string
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function is($urlName = '')
+	{
+		/*
+			Getting the current url and trimming trailing slashes
+		*/
+		$url = $this->url();
+		$url = trim($url, '/');
+
+		/*
+			Getting position of * symbol from $urlName and Length of the $urlName
+		*/
+		$pos = strpos($urlName, "*");
+		$length = strlen($urlName);
+
+		/*
+			Trimming trailing slashes
+		*/
+		$urlName = trim($urlName, '*');
+		$urlName = trim($urlName, '/');
+
+		/*
+			If the * symbol is not given then it matches with exactly what it is
+		*/
+		if ($pos === false) {
+			return ($url === $urlName);
+		}
+
+		/*
+			If the * symbol found at end of the $urlName then 
+			It matches whether it start with value of $urlName string to the $url
+		*/
+		if (($pos+1) === $length) {
+			return strpos($url, $urlName) === 0;
+		}
+
+		/*
+			If the * symbol found at starting of the $urlName then 
+			It matches whether it end with value of $urlName string to the $url
+		*/
+		if (($pos) === 0) {
+			return strpos($url, $urlName) > 0;	
+		}
+
+	}
+
+	/**
+	 * This will collect ip address of requested user and returns it
+	 *
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return ipAddress
+	 *
+	 */
+	public function ip()
+	{
+		if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
+			return $_SERVER['HTTP_CLIENT_IP'];
+		}
+
+		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			return $_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+
+		if (isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR'])) {
+			return $_SERVER['REMOTE_ADDR'];
+		}
+
+		return null;
+	}
+
+	/**
+	 * This will get information about method of the request
+	 *
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return method
+	 *
+	 */
+	public function method()
 	{
 		return strtolower($_SERVER['REQUEST_METHOD']);
 	}
@@ -93,56 +251,298 @@ class Request
 	/**
 	 * This will checks whether it is get method or not
 	 *
-	 * @return boolean
 	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return boolean
 	 * 
 	 */
 	public function isGet()
 	{
-		return $this->methodRequest() === 'get';
+		return $this->method() === 'get';
 	}
 
 	/**
 	 * This will checks whether it is post method or not
 	 *
-	 * @return boolean
 	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return boolean
 	 * 
 	 */
 	public function isPost()
 	{
-		return $this->methodRequest() === 'post';
+		return $this->method() === 'post';
+	}	
+
+	/**
+	 * This will return collected input as per requirement
+	 *
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return $data
+	 *
+	 */
+	public function input($inputName='')
+	{
+		$data = self::$input;
+
+		/*
+			If any inputName request and that value will be return
+			otherwise returns null
+		*/
+		if (!empty($inputName)) {
+			return $data[$inputName] ?? null;	
+		}
+
+		return $data;
 	}
 
 	/**
-	 * This will collect all data from the request
+	 * This will return only specific inputs as per requirements
 	 *
-	 * @return inputs
+	 * @param inputNames $onlyInputs This is combination input names
 	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return array	 
 	 *
 	 */
-	public function inputsFromRequest()
+	public function only($onlyInputs)
 	{
-		$data = [];
+		$data = array();
+		$input = self::$input;
 
-		/*
-			collects all data if it is a get request method
-		*/
-		if ($this->isGet()) {
-			foreach ($_GET as $key => $value) {
-				$data[$key] = $value;
-			}
+		if (is_string($onlyInputs)) {
+			$onlyInputs = explode(',', $onlyInputs);
 		}
 
-		/*
-			collects all data if it is a post request method
-		*/
-		if ($this->isPost()) {
-			foreach ($_POST as $key => $value) {
-				$data[$key] = $value;
+		foreach ($onlyInputs as $key => $value) {
+			$keyValue = trim($value);
+			if (array_key_exists($keyValue, $input)) {
+				$data[$keyValue] = $input[$keyValue];
 			}
 		}
 
 		return $data;
 	}
+
+	/**
+	 * This will return except specific inputs as per requirements
+	 *
+	 * @param inputNames $exceptInputs This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return array	 
+	 *
+	 */
+	public function except($exceptInputs = '')
+	{
+		$input = self::$input;
+
+		/*
+			If $exceptInputs is not present then it will return all input
+		*/
+		if (empty($exceptInputs)) {
+			return $input;
+		}
+
+		if (is_string($exceptInputs)) {
+			$exceptInputs = explode(',', $exceptInputs);
+		}
+
+		/*
+			If any of the $exceptInputs presents in the $input keys then
+			It will remove from $input
+		*/
+		foreach ($exceptInputs as $key => $value) {
+			$keyValue = trim($value);
+			if (array_key_exists($keyValue, $input)) {
+				unset($input[$keyValue]);
+			}
+		}
+
+		return $input;
+	}
+
+	/**
+	 * This will check for all of the input key exist in the input
+	 *
+	 * @param InputNames $hasInput This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function has($hasInput)
+	{
+		$input = self::$input;
+
+		if (is_string($hasInput)) {
+			$hasInput = explode(',', $hasInput);
+		}
+
+		foreach ($hasInput as $key => $value) {
+			$keyValue = trim($value);
+			if (!array_key_exists($keyValue, $input)) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * This will check for any of the input key exist in the input
+	 *
+	 * @param InputNames $hasInput This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function hasAny($hasInput)
+	{
+		$input = self::$input;
+
+		if (is_string($hasInput)) {
+			$hasInput = explode(',', $hasInput);
+		}
+
+		foreach ($hasInput as $key => $value) {
+			$keyValue = trim($value);
+			if (array_key_exists($keyValue, $input)) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * This will check for all of the input is not an empty
+	 * And if the input name is not exist in the $input then it will be false
+	 *
+	 * @param InputNames $inputName This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function filled($inputName)
+	{
+		$input = self::$input;
+
+		if (is_string($inputName)) {
+			$inputName = explode(',', $inputName);
+		}
+		
+		foreach ($inputName as $key => $value) {
+			$keyValue = trim($value);
+			
+			if (isset($input[$keyValue]) && empty($input[$keyValue])) {
+				return false;
+			} elseif(!isset($input[$keyValue])) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * This will check for any of the input is not an empty
+	 * And if the input name is not exist in the $input then it will be false
+	 *
+	 * @param InputNames $inputName This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function filledAny($inputName)
+	{
+		$input = self::$input;
+
+		if (is_string($inputName)) {
+			$inputName = explode(',', $inputName);
+		}
+		
+		foreach ($inputName as $key => $value) {
+			$keyValue = trim($value);
+			
+			if (isset($input[$keyValue]) && !empty($input[$keyValue])) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * This will check for any of the input key is not exist in the input
+	 *
+	 * @param InputNames $missingInput This is combination of input names
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool
+	 *
+	 */
+	public function missing($missingInput)
+	{
+		$input = self::$input;
+
+		if (is_string($missingInput)) {
+			$missingInput = explode(',', $missingInput);
+		}
+
+		foreach ($missingInput as $key => $value) {
+			$keyValue = trim($value);
+			if (!array_key_exists($keyValue, $input)) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Get all acceptable content types for the request
+	 *
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return array 
+	 *
+	 */
+	public function acceptableContentTypes()
+	{
+		$contentTypes = $_SERVER['HTTP_ACCEPT'] ?? '';
+
+		return explode(',', $contentTypes);
+
+	}
+
+	/**
+	 * It will checks whether the request accepts given contentTypes
+	 *
+	 * @param contents $contentTypes The combination content types
+	 * @author ag-sanjjeev <sanjjeevag.aug21@gmail.com>
+	 * @return bool 
+	 *
+	 */
+	public function accepts($contentTypes)
+	{
+		$acceptables = $_SERVER['HTTP_ACCEPT'] ?? '';
+		$acceptables = explode(',', $acceptables);
+		
+		if (is_string($contentTypes)) {
+			$contentTypes = explode(',', $contentTypes);
+		}
+
+		/*
+			If any of the $contentTypes is not present in $acceptable then 
+			it will return false
+		*/
+		foreach ($contentTypes as $key => $value) {
+			if (!in_array(trim($value), $acceptables)) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
 }
