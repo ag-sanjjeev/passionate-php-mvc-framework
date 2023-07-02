a7e4c1818eb5111924c16c31439a6a84c3d07393
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2023-07-02 15:48:36 +0530

    Changing the ui for error pages

--- public/views/default/403_error.php
+++ public/views/default/403_error.php
@@ -1,5 +1,55 @@
-<?php
-	
-	echo "403 forbidden";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>403 - Forbidden Access</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background: linear-gradient(135deg, #667eea, #764ba2);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
 
-?>
+    .container {
+      background-color: rgba(255, 255, 255, 0.9);
+      border-radius: 10px;
+      padding: 40px;
+      text-align: center;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    h1 {
+      font-size: 48px;
+      margin-bottom: 20px;
+      color: #333;
+    }
+
+    p {
+      font-size: 24px;
+      margin-bottom: 20px;
+      color: #555;
+    }
+
+    a {
+      color: #007bff;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>403</h1>
+    <p>Forbidden Access</p>
+    <p>Sorry, the page you are looking for is not accessible.</p>
+    <a href="/">Go back to the homepage</a>
+  </div>
+</body>
+</html>
--- public/views/default/404_error.php
+++ public/views/default/404_error.php
@@ -1,5 +1,55 @@
-<?php
-	
-	echo "404 page not found";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>404 - Page Not Found</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background: linear-gradient(135deg, #667eea, #764ba2);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
 
-?>
+    .container {
+      background-color: rgba(255, 255, 255, 0.9);
+      border-radius: 10px;
+      padding: 40px;
+      text-align: center;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    h1 {
+      font-size: 48px;
+      margin-bottom: 20px;
+      color: #333;
+    }
+
+    p {
+      font-size: 24px;
+      margin-bottom: 20px;
+      color: #555;
+    }
+
+    a {
+      color: #007bff;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>404</h1>
+    <p>Page Not Found</p>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <a href="/">Go back to the homepage</a>
+  </div>
+</body>
+</html>
